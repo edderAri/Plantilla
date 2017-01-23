a635f533e6de072d6ebaf028530a5c5003548a7c
edderAri <edder.1022@gmail.com>
2017-01-23 21:20:52 +0100

    Led Pot.

---
+++ LED_Potenciometre/LED_Potenciometre.ino
@@ -0,0 +1,27 @@
+/****************************************************************
+**                                                             **
+**                           TÍTOL: LED Potenciometre          **      
+**                                                             **
+**                                                             **
+**  NOM: EDDER GARCIA PINO                     DATA 23/01/2017 **
+****************************************************************/
+//************************ INCLUDE ******************************
+
+
+//*********************** VARIABLES *****************************
+int sensorValue = 0; /*Variable valor potenciometre*/
+
+//************************* SETUP *******************************
+
+void setup() {               // configura el final de salida
+ pinMode(3, OUTPUT);
+}
+
+//************************** LOOP *******************************
+
+void loop() {                // inicia el bucle del programa
+ sensorValue = analogRead(A0);
+ analogWrite(3, sensorValue/4);
+}
+
+//************************* FUNCIONS ****************************
