df8ab1f9dd7cf0e8c7ae0acda054e3846b1eec50
edderAri <edder.1022@gmail.com>
2017-01-23 16:51:44 +0100

    Blink

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -21,9 +21,11 @@ void setup() {               // configura el final de salida
 //************************** LOOP *******************************
 
 void loop() {                // inicia el bucle del programa
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);   /* turn the LED on (HIGH 
+  is the voltage level)*/
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);    /* turn the LED off by
+  making the voltage LOW*/
   delay(1000);                       // wait for a second 
 }
 
